33f14d96a48f6a9c373bd2f79d0c5e8ee909cc53
sohanemon <m.sohanemon@gmail.com>
2023-05-02 23:59:50 +0600

    refactor(tailwind.config.js): remove unused background image styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       colors: {
         primary: '#ed6a5a',
         secondary: '#77b6ea',
