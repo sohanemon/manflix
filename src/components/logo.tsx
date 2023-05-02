1eeb29e405fd562c07a912155f62f9e57ad9f7cb
sohanemon <m.sohanemon@gmail.com>
2023-05-02 23:27:18 +0600

    feat(logo.tsx): add Logo component to display the MANFLIX logo and slogan

---
+++ src/components/logo.tsx
@@ -0,0 +1,10 @@
+export default function Logo() {
+  return (
+    <div className='w-max'>
+      <h1 className='flex items-center font-bold text-3xl justify-between'>
+        <p>MAN </p> <p className='text-primary'>FLIX</p>
+      </h1>
+      <p className='tracking-wider text-secondary leading-3'>Awesome Videos</p>
+    </div>
+  );
+}
