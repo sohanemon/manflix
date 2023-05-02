575ac5f83c30d64a2e1730b0b80653e3229108ee
sohanemon <m.sohanemon@gmail.com>
2023-05-02 15:53:56 +0600

    feat(store.ts): add store configuration with counterSlice reducer and RootState and AppDispatch types

---
+++ src/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterSlice from './slices/counter-slice';
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice,
+  },
+});
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
