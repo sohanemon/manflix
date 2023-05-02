81bbccf03d280c75b02a9cd33f49e5adafdd13ab
sohanemon <m.sohanemon@gmail.com>
2023-05-02 15:55:43 +0600

    refactor(redux): remove Redux component and counter-slice reducer refactor(store.ts): remove counter-slice reducer from store configuration

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterSlice from './slices/counter-slice';
 
 const store = configureStore({
-  reducer: {
-    counter: counterSlice,
-  },
+  reducer: {},
 });
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
