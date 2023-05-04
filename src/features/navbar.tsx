a0c1158347945f8061a9494565275d2275ff0c70
sohanemon <m.sohanemon@gmail.com>
2023-05-04 13:23:09 +0600

    search added

--- src/features/navbar.tsx
+++ src/features/navbar.tsx
@@ -1,9 +1,12 @@
 import Logo from '@/components/logo';
+import Search from '@/components/search';
 
 export default function Navbar() {
   return (
-    <div className='container mx-auto'>
+    <div className='w-5/6 mx-auto my-4 flex items-center justify-between'>
       <Logo />
+      <Search />
+      <h1>woo hoo</h1>
     </div>
   );
 }
