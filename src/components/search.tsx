a8df65f9765172d2276eb688767375949f375b12
sohanemon <m.sohanemon@gmail.com>
2023-05-04 14:08:49 +0600

    headlessui added

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,39 +1,9 @@
 'use client';
-import * as Select from '@radix-ui/react-select';
-import { BsChevronCompactDown, BsSearch } from 'react-icons/bs';
+import { BsSearch } from 'react-icons/bs';
 
 export default function Search() {
   return (
     <div className='flex items-center px-5 border border-gray-600 focus-within:border-primary rounded-full group'>
-      <div>
-        <Select.Root>
-          <Select.Trigger
-            aria-label='Category'
-            className='flex items-center gap-1 border-r border-gray-600 pr-2 group-focus-within:border-primary'
-          >
-            <Select.Value placeholder='Category' />
-            <Select.Icon>
-              <BsChevronCompactDown />
-            </Select.Icon>
-          </Select.Trigger>
-          <Select.Portal>
-            <Select.Content>
-              <Select.ScrollUpButton>up</Select.ScrollUpButton>
-              <Select.Viewport>
-                <Select.Group>
-                  <Select.Label>Fruits</Select.Label>
-                  <p>Apple</p>
-                  <p>Banana</p>
-                  <p>Blueberry</p>
-                  <p>Grapes</p>
-                  <p>Pineapple</p>
-                </Select.Group>
-              </Select.Viewport>
-              <Select.ScrollDownButton>down</Select.ScrollDownButton>
-            </Select.Content>
-          </Select.Portal>
-        </Select.Root>
-      </div>
       <Input />
     </div>
   );
