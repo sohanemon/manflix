968983e7c8449b40ce971119e904dcb62f668cef
sohanemon <m.sohanemon@gmail.com>
2023-05-12 16:35:27 +0000

    Invalid DateTime

--- src/app/api/v2/video/route.ts
+++ src/app/api/v2/video/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import { JSDOM } from 'jsdom';
 import thumbnailToId from '@/util/thumbnail-to-id';
 
-export async function GET(req: Request) {
+export async function POST(req: Request) {
   const data = await req.json();
   console.log('🛑 ~ GET ~ data:', data);
 
