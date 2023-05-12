57fbb34c4530a67fb8545e2365f3b9c79422dadd
sohanemon <m.sohanemon@gmail.com>
2023-05-12 16:25:19 +0600

    link added

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link';
+
 export default function Logo() {
   return (
-    <div className='w-max'>
+    <Link href={'/'} className='w-max'>
       <h1 className='flex items-center font-bold text-3xl justify-between'>
         <p>MAN </p> <p className='text-primary-500'>FLIX</p>
       </h1>
       <p className='tracking-wider text-secondary-500 leading-3'>
         Awesome Videos
       </p>
-    </div>
+    </Link>
   );
 }
