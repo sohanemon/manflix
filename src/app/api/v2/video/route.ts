11a0927b842e162009384cbb9e27366ef0516437
sohanemon <m.sohanemon@gmail.com>
2023-05-12 16:33:42 +0000

    Invalid DateTime

--- src/app/api/v2/video/route.ts
+++ src/app/api/v2/video/route.ts
@@ -4,6 +4,9 @@ import { JSDOM } from 'jsdom';
 import thumbnailToId from '@/util/thumbnail-to-id';
 
 export async function GET(req: Request) {
+  const data = await req.json();
+  console.log('🛑 ~ GET ~ data:', data);
+
   const response = await fetch('https://www.genyt.com/search.php?q=athlean+x');
   const html = await response.text();
   const {
