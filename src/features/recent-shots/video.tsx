9784b9d0e76027370ebf666f11f5634aa10bdecd
sohanemon <m.sohanemon@gmail.com>
2023-05-12 14:44:42 +0600

    card section style randomized

--- src/features/recent-shots/video.tsx
+++ src/features/recent-shots/video.tsx
@@ -1,14 +1,17 @@
+import { chooseOpt } from '@/util/choose-opt';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Video({ thumbnail, title, author }: Video) {
+export default function Video({ thumbnail, title, author, duration }: Video) {
   return (
     <Link
       href={'/'}
-      className='relative w-full max-w-xs text-black bg-white group'
+      className='relative w-full max-w-xs overflow-hidden text-black transition-all bg-white rounded-lg group hover:rounded-none '
     >
       <Image
-        className='w-full aspect-video'
+        className={`w-full transition-all duration-500 group-hover:${
+          chooseOpt() ? '-' : ''
+        }rotate-2 group-hover:brightness-50 group-hover:scale-110 aspect-video`}
         src={thumbnail!}
         alt={title + ' - thumbnail'}
         width={300}
@@ -16,6 +19,10 @@ export default function Video({ thumbnail, title, author }: Video) {
       />
       <div className='absolute inset-x-0 bottom-0 hidden p-2 pt-10 font-medium text-white group-hover:block bg-gradient-to-t from-black via-black/75 to-transparent'>
         <h2>{title}</h2>
+        <div className='flex justify-between text-xs text-neutral-300'>
+          <h3 className=''>{author}</h3>
+          <p>{duration}</p>
+        </div>
       </div>
     </Link>
   );
---
+++ src/util/choose-opt.ts
@@ -0,0 +1,5 @@
+export function chooseOpt() {
+  const num = Math.random();
+  if (num >= 0.5) return 1;
+  else return 0;
+}
