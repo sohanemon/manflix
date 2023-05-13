ed306579f38bc582805caed1de0b272cfe974423
sohanemon <m.sohanemon@gmail.com>
2023-05-13 09:59:36 +0000

    May 13, 2023, 3:59 PM

--- src/features/recent-shots/index.tsx
+++ src/features/recent-shots/index.tsx
@@ -3,7 +3,7 @@ import { videoFetcher } from '@/lib/fetcher';
 import VideoCard from '../../components/video-card';
 import ShowMore from './show-more';
 import { use } from 'react';
-import { selectVideos } from '@/slices/video';
+import video, { selectVideos } from '@/slices/video';
 import { useSelector } from 'react-redux';
 
 export default function Index() {
@@ -11,10 +11,13 @@ export default function Index() {
   const searchedVideos = useSelector(selectVideos);
   return (
     <div className='grid grid-cols-1 gap-5 mx-auto sm:grid-cols-2 md:grid-cols-3 justify-items-evenly lg:w-3/4'>
-      {!searchedVideos.length &&
-        videos?.map((video) => (
-          <VideoCard key={video.id} {...video}></VideoCard>
-        ))}
+      {!searchedVideos.length
+        ? videos?.map((video) => (
+            <VideoCard key={video.id} {...video}></VideoCard>
+          ))
+        : searchedVideos?.map((video) => (
+            <VideoCard key={video.id} {...video}></VideoCard>
+          ))}
       <ShowMore />
     </div>
   );
