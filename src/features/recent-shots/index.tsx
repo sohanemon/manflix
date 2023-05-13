c3bb9aa85824ce824a62fc378cd327efe02cc712
sohanemon <m.sohanemon@gmail.com>
2023-05-13 09:55:52 +0000

    May 13, 2023, 3:55 PM

--- src/features/recent-shots/index.tsx
+++ src/features/recent-shots/index.tsx
@@ -1,9 +1,11 @@
+'use client';
 import { videoFetcher } from '@/lib/fetcher';
 import VideoCard from '../../components/video-card';
 import ShowMore from './show-more';
+import { use } from 'react';
 
-export default async function Index() {
-  const videos = await videoFetcher('');
+export default function Index() {
+  const videos = use(videoFetcher('abdullah bin abdur razzak new'));
   return (
     <div className='grid grid-cols-1 gap-5 mx-auto sm:grid-cols-2 md:grid-cols-3 justify-items-evenly lg:w-3/4'>
       {videos?.map((video) => (
--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,14 +1,11 @@
 export async function videoFetcher(payload: any) {
-  const res = await fetch(
-    'http://localhost:3000/api/v2/video?query=abdullah bin abdur razzak new',
-    {
-      next: { revalidate: 50 },
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+  const res = await fetch(`/api/v2/video?query=${payload}`, {
+    next: { revalidate: 50 },
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
   const videos: Video[] = await res.json();
   return videos;
 }
