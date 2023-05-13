1d03dd8ce144007433f5932f9daa7b1a933c37e5
sohanemon <m.sohanemon@gmail.com>
2023-05-13 10:18:07 +0000

    May 13, 2023, 4:18 PM

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,7 +1,7 @@
 export async function videoFetcher(payload: any) {
   const url = 'http://localhost:3000';
   const res = await fetch(`${url}/api/v2/video?query=${payload}`, {
-    next: { revalidate: 500 },
+    // next: { revalidate: 500 },
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
