cf07d6baf19d0d630070d3349a24109a0b7ab077
sohanemon <m.sohanemon@gmail.com>
2023-05-13 09:58:31 +0000

    May 13, 2023, 3:58 PM

--- src/features/recent-shots/index.tsx
+++ src/features/recent-shots/index.tsx
@@ -3,14 +3,18 @@ import { videoFetcher } from '@/lib/fetcher';
 import VideoCard from '../../components/video-card';
 import ShowMore from './show-more';
 import { use } from 'react';
+import { selectVideos } from '@/slices/video';
+import { useSelector } from 'react-redux';
 
 export default function Index() {
   const videos = use(videoFetcher('abdullah bin abdur razzak new'));
+  const searchedVideos = useSelector(selectVideos);
   return (
     <div className='grid grid-cols-1 gap-5 mx-auto sm:grid-cols-2 md:grid-cols-3 justify-items-evenly lg:w-3/4'>
-      {videos?.map((video) => (
-        <VideoCard key={video.id} {...video}></VideoCard>
-      ))}
+      {!searchedVideos.length &&
+        videos?.map((video) => (
+          <VideoCard key={video.id} {...video}></VideoCard>
+        ))}
       <ShowMore />
     </div>
   );
