99b132045c5db4842c71fd1f4384ab74715991c0
sohanemon <m.sohanemon@gmail.com>
2023-05-13 06:50:15 +0000

    May 13, 2023, 12:50 PM

--- src/slices/video.ts
+++ src/slices/video.ts
@@ -4,26 +4,10 @@ const initialState = {
   videos: [],
 };
 
-const fetchVideosThunk = createAsyncThunk('fetch-videos', async (payload) => {
-  const url =
-    'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk';
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '1df37e59aemshcfc14c3df1f5426p1361d2jsnfdfa8460b0b2',
-      'X-RapidAPI-Host':
-        'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
-    },
-  };
-
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
-});
+const fetchVideosThunk = createAsyncThunk(
+  'fetch-videos',
+  async (payload) => {}
+);
 
 const videoSlice = createSlice({
   name: 'videos',
