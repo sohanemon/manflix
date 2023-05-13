3df2e2269477d4079a4063bc35faaf5a5f2210ba
sohanemon <m.sohanemon@gmail.com>
2023-05-13 09:52:37 +0000

    May 13, 2023, 3:52 PM

--- src/features/recent-shots/index.tsx
+++ src/features/recent-shots/index.tsx
@@ -1,11 +1,8 @@
-import { selectVideos } from '@/slices/video';
-import { useSelector } from 'react-redux';
+import { videoFetcher } from '@/lib/fetcher';
 import VideoCard from '../../components/video-card';
 import ShowMore from './show-more';
-import { videoFetcher } from '@/lib/fetcher';
 
 export default async function Index() {
-  // const videos = useSelector(selectVideos);
   const videos = await videoFetcher('');
   return (
     <div className='grid grid-cols-1 gap-5 mx-auto sm:grid-cols-2 md:grid-cols-3 justify-items-evenly lg:w-3/4'>
