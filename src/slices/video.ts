dba33dc7c3011ed015e421cc6c7dc621e4fbccd9
sohanemon <m.sohanemon@gmail.com>
2023-05-13 06:51:44 +0000

    May 13, 2023, 12:51 PM

--- src/slices/video.ts
+++ src/slices/video.ts
@@ -4,15 +4,28 @@ const initialState = {
   videos: [],
 };
 
-const fetchVideosThunk = createAsyncThunk(
-  'fetch-videos',
-  async (payload) => {}
-);
+const fetchVideosThunk = createAsyncThunk('fetch-videos', async (payload) => {
+  const res = await fetch(
+    'http://localhost:3000/api/v2/video?query=abdullah bin abdur razzak new',
+    {
+      next: { revalidate: 50 },
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+  const videos: Video[] = await res.json();
+});
 
 const videoSlice = createSlice({
   name: 'videos',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchVideosThunk.fulfilled, reducer);
+  },
 });
 
 export const {} = videoSlice.actions;
