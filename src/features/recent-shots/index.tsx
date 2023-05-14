16979af8b5946859649b01397b86d22c4b81e9de
sohanemon <m.sohanemon@gmail.com>
2023-05-14 04:27:06 +0000

    May 14, 2023, 10:27 AM

--- src/features/recent-shots/index.tsx
+++ src/features/recent-shots/index.tsx
@@ -1,27 +1,30 @@
 'use client';
 
 import { videoFetcher } from '@/lib/fetcher';
-import { selectVideos } from '@/slices/video';
-import { use } from 'react';
-import { useSelector } from 'react-redux';
+import { fetchVideosThunk, selectVideos } from '@/slices/video';
+import { use, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import VideoCard from '../../components/video-card';
 import ShowMore from './show-more';
+import { AppDispatch } from '@/store';
 
 export default function Index() {
-  const videos = use(videoFetcher('abdullah bin abdur razzak new'));
-  const searchedVideos = useSelector(selectVideos);
+  // const videos = use(videoFetcher('abdullah bin abdur razzak new'));
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    dispatch(fetchVideosThunk('abdullah bin abdur razzak 2023'));
 
-  console.log('🛑 ~ Index ~ searchedVideos:', searchedVideos);
+    return () => {};
+  }, []);
+
+  const videos = useSelector(selectVideos);
 
   return (
     <div className='grid grid-cols-1 gap-5 mx-auto sm:grid-cols-2 md:grid-cols-3 justify-items-evenly lg:w-3/4'>
-      {!searchedVideos.length
-        ? videos?.map((video) => (
-            <VideoCard key={video.id} {...video}></VideoCard>
-          ))
-        : searchedVideos?.map((video) => (
-            <VideoCard key={video.id} {...video}></VideoCard>
-          ))}
+      {videos?.map((video) => (
+        <VideoCard key={video.id} {...video}></VideoCard>
+      ))}
+
       <ShowMore />
     </div>
   );
