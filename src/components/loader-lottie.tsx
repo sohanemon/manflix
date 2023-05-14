1d7fc670251c1117ee029791edd991dc83ec271d
sohanemon <m.sohanemon@gmail.com>
2023-05-14 14:08:33 +0000

    May 14, 2023, 8:08 PM

--- src/components/loader-lottie.tsx
+++ src/components/loader-lottie.tsx
@@ -1,13 +1,17 @@
 'use client';
-import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import { Player } from '@lottiefiles/react-lottie-player';
 
-export default function LoaderLottie() {
+interface Props {
+  className: string;
+}
+
+export default function LoaderLottie({ className }: Props) {
   return (
     <>
       <Player
         autoplay
         loop
-        className='sticky inset-0 w-48 my-auto aspect-square'
+        className={`sticky inset-0 ${className} my-auto aspect-square`}
         src={require('../assets/lottie/samurai.json')}
       ></Player>
     </>
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,14 +1,18 @@
 import Link from 'next/link';
+import LoaderLottie from './loader-lottie';
 
 export default function Logo() {
   return (
-    <Link href={'/'} className='w-max'>
-      <h1 className='flex items-center justify-between text-3xl font-bold'>
-        <p>MAN </p> <p className='text-primary-500'>FLIX</p>
-      </h1>
-      <p className='leading-3 tracking-wider text-secondary-500'>
-        Awesome Videos
-      </p>
+    <Link href={'/'} className='flex items-center w-max'>
+      <LoaderLottie className='w-14' />
+      <div className='flex flex-col'>
+        <h1 className='flex items-center justify-between text-3xl font-bold'>
+          <p>MAN </p> <p className='text-primary-500'>FLIX</p>
+        </h1>
+        <p className='leading-3 tracking-wider text-secondary-500'>
+          Awesome Videos
+        </p>
+      </div>
     </Link>
   );
 }
