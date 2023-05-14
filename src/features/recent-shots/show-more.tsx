e554550f44d9b6502847cb63273b3c7e0a22428f
sohanemon <m.sohanemon@gmail.com>
2023-05-14 13:55:47 +0000

    May 14, 2023, 7:55 PM

--- src/features/recent-shots/show-more.tsx
+++ src/features/recent-shots/show-more.tsx
@@ -1,20 +1,11 @@
 'use client';
 
-import { selectVideos, triggerSearch } from '@/slices/video';
-import { AppDispatch } from '@/store';
-import { useDispatch, useSelector } from 'react-redux';
-
 export default function ShowMore() {
-  const searchedVideos = useSelector(selectVideos);
-  const dispatch = useDispatch<AppDispatch>();
-  console.log('🛑 ~ ShowMore ~ searchedVideos:', searchedVideos);
-
   return (
     <>
       <div
         onClick={() => {
           window.alert('Why are you so greedy?');
-          dispatch(triggerSearch(true));
         }}
         className='grid w-full h-full text-xl font-semibold tracking-wider transition-all cursor-pointer place-content-center active:scale-95'
       >
