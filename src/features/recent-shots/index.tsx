491111533b342677db724a04423e8df6e2578115
sohanemon <m.sohanemon@gmail.com>
2023-05-14 06:51:57 +0000

    May 14, 2023, 12:51:57 PM

--- src/features/recent-shots/index.tsx
+++ src/features/recent-shots/index.tsx
@@ -6,7 +6,7 @@ import {
   selectVideos,
 } from '@/slices/video';
 import { AppDispatch } from '@/store';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import VideoCard from '../../components/video-card';
 import ShowMore from './show-more';
@@ -25,12 +25,14 @@ export default function Index() {
   if (isPending) return <LoaderLottie />;
 
   return (
-    <div className='grid grid-cols-1 gap-5 mx-auto sm:grid-cols-2 md:grid-cols-3 justify-items-evenly lg:w-3/4'>
-      {videos?.map((video) => (
-        <VideoCard key={video.id} {...video}></VideoCard>
-      ))}
-      {/* <ShowMore /> */}
-      <VideoCardSkeletons />
-    </div>
+    <Suspense fallback={<VideoCardSkeletons />}>
+      <div className='grid grid-cols-1 gap-5 mx-auto sm:grid-cols-2 md:grid-cols-3 justify-items-evenly lg:w-3/4'>
+        {videos?.map((video) => (
+          <VideoCard key={video.id} {...video}></VideoCard>
+        ))}
+        {/* <ShowMore /> */}
+        <VideoCardSkeletons />
+      </div>
+    </Suspense>
   );
 }
