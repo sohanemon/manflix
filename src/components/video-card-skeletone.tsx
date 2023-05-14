d37b414b10f9ae796f15ff4a0a09fa928704760d
sohanemon <m.sohanemon@gmail.com>
2023-05-14 12:48:11 +0600

    skaletone done

--- src/components/video-card-skeletone.tsx
+++ src/components/video-card-skeletone.tsx
@@ -1,11 +1,12 @@
 export default function VideoCardSkeletons() {
   return (
-    <div className='relative w-full max-w-xs overflow-hidden text-black transition-all rounded-lg bg-neutral-700 group hover:rounded-none '>
-      <div className='absolute inset-x-0 bottom-0 p-2 pt-10 font-medium text-white bg-gradient-to-t from-black via-black/75 to-transparent'>
-        <h2>lofdkjf</h2>
-        <div className='flex justify-between text-xs text-neutral-300'>
-          <h3 className=''>sfsfkjsaklfaksfjklasjfsa</h3>
-          <p>sfasjfklasjfklasjkfjsaklfj</p>
+    <div className='relative w-full max-w-xs overflow-hidden text-black transition-all rounded-lg bg-neutral-700 group hover:rounded-none [&_p]:animate-pulse'>
+      <div className='absolute inset-x-0 bottom-0 p-4 pt-10 space-y-2 font-medium text-white '>
+        <p className='w-full h-5 rounded-md bg-neutral-500' />
+        <p className='w-2/3 h-5 rounded-md bg-neutral-500' />
+        <div className='flex justify-between'>
+          <p className='w-20 h-3 rounded-md bg-neutral-500' />
+          <p className='w-16 h-3 rounded-md bg-neutral-500' />
         </div>
       </div>
     </div>
