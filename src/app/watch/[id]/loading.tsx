872f44e82e9adeb9ca19ef41d693775392caeee7
sohanemon <m.sohanemon@gmail.com>
2023-05-14 12:19:36 +0000

    May 14, 2023, 6:19 PM

--- src/app/watch/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import LoaderLottie from '@/components/loader-lottie';
-
-export default function Loading() {
-  return (
-    <>
-      <LoaderLottie />
-    </>
-  );
-}
--- src/app/watch/[id]/page.tsx
+++ src/app/watch/[id]/page.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from 'react';
 import Description from './description';
 import Title from './title';
+import LoaderLottie from '@/components/loader-lottie';
 
 export default async function Page({ params: { id } }: any) {
   const res = await fetch('http://localhost:3000/api/v2/player', {
@@ -9,23 +11,25 @@ export default async function Page({ params: { id } }: any) {
   const videoDetails = (await res.json()) as VideoDetails;
 
   return (
-    <section className='w-full mx-auto md:w-3/4 lg:w-3/5 '>
-      <iframe
-        className='w-full mx-auto aspect-video'
-        src={videoDetails.link}
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-      ></iframe>
-      <div className='mt-4'>
-        <Title title={videoDetails.title}></Title>
-        <div className='flex justify-between'>
-          <p className='text-primary-500'>{videoDetails.author}</p>
-          <p>
-            {videoDetails.views} views - {videoDetails.publishedAt}
-          </p>
+    <Suspense fallback={<LoaderLottie />}>
+      <section className='w-full mx-auto md:w-3/4 lg:w-3/5 '>
+        <iframe
+          className='w-full mx-auto aspect-video'
+          src={videoDetails.link}
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        ></iframe>
+        <div className='mt-4'>
+          <Title title={videoDetails.title}></Title>
+          <div className='flex justify-between'>
+            <p className='text-primary-500'>{videoDetails.author}</p>
+            <p>
+              {videoDetails.views} views - {videoDetails.publishedAt}
+            </p>
+          </div>
+          <br />
+          <Description description={videoDetails.description!} />
         </div>
-        <br />
-        <Description description={videoDetails.description!} />
-      </div>
-    </section>
+      </section>
+    </Suspense>
   );
 }
