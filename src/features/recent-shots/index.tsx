f658462665186d0a6cbf81800849e16ca45fb41a
sohanemon <m.sohanemon@gmail.com>
2023-05-14 13:40:41 +0000

    May 14, 2023, 7:40 PM

--- src/features/recent-shots/index.tsx
+++ src/features/recent-shots/index.tsx
@@ -5,11 +5,12 @@ import { selectQueryState, selectVideos } from '@/slices/video';
 import { useSelector } from 'react-redux';
 import VideoCard from '../../components/video-card';
 import ShowMore from './show-more';
+import useFetcher from '@/hooks/use-fetcher';
 
 export default function Index() {
   const videos = useSelector(selectVideos);
   const [isPending, isError] = useSelector(selectQueryState);
-
+  useFetcher();
   return (
     <div className='grid grid-cols-1 gap-5 mx-auto sm:grid-cols-2 md:grid-cols-3 justify-items-center lg:w-3/4'>
       {isPending
--- src/hooks/use-fetcher.tsx
+++ src/hooks/use-fetcher.tsx
@@ -1,14 +1,13 @@
-'use client';
-
 import { fetchVideosThunk } from '@/slices/video';
 import { AppDispatch } from '@/store';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
-export default function Fetcher() {
+const useFetcher = () => {
   const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
     dispatch(fetchVideosThunk('trending web development technology'));
   }, [dispatch]);
-  return null;
-}
+};
+
+export default useFetcher;
