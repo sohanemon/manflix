7c4dea22be4cb430220bb6f482643d4de8145fc5
sohanemon <m.sohanemon@gmail.com>
2023-05-14 14:18:34 +0000

    May 14, 2023, 8:18 PM

---
+++ src/app/submit/layout.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: 'Submit Video',
+  description: 'Add video to the home page carousel',
+};
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return children;
+}
