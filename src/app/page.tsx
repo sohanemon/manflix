bde8400df24ef0d4074cea0b3c290c8599b05278
sohanemon <m.sohanemon@gmail.com>
2023-05-14 12:38:36 +0000

    May 14, 2023, 6:38 PM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/features/hero';
 import Navbar from '@/features/navbar';
 import RecentShots from '@/features/recent-shots';
 import { Inter } from 'next/font/google';
+import Fetcher from './fetcher';
 
 const inter = Inter({ subsets: ['latin'] });
 
