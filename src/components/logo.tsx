ec98fdacd194fa9800fe89cf499b93af46d16fee
sohanemon <m.sohanemon@gmail.com>
2023-05-14 15:44:46 +0000

    May 14, 2023, 9:44 PM

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,11 +5,11 @@ export default function Logo() {
   return (
     <Link href={'/'} className='flex items-center w-max '>
       <LoaderLottie className='w-14' />
-      <div className='flex flex-col'>
-        <h1 className='flex items-center justify-between text-3xl font-bold sm:text-2xl md:text-3xl'>
+      <div className='flex flex-col '>
+        <h1 className='flex items-center justify-between text-2xl font-bold '>
           <p>MAN </p> <p className='text-primary-500'>FLIX</p>
         </h1>
-        <p className='leading-3 tracking-wider text-secondary-500'>
+        <p className='text-sm leading-3 tracking-wider line-clamp-1 text-secondary-500'>
           Awesome Videos
         </p>
       </div>
