50bf9fa47685fee2e0f05d28bbd21bd0383c4a82
sohanemon <m.sohanemon@gmail.com>
2023-05-14 13:41:44 +0000

    May 14, 2023, 7:41 PM

--- src/hooks/use-fetcher.tsx
+++ src/hooks/use-fetcher.tsx
@@ -1,13 +1,15 @@
-import { fetchVideosThunk } from '@/slices/video';
+import { fetchVideosThunk, selectVideos } from '@/slices/video';
 import { AppDispatch } from '@/store';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const useFetcher = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const videos = useSelector(selectVideos);
   useEffect(() => {
-    dispatch(fetchVideosThunk('trending web development technology'));
-  }, [dispatch]);
+    if (!videos.length)
+      dispatch(fetchVideosThunk('trending web development technology'));
+  }, [dispatch, videos.length]);
 };
 
 export default useFetcher;
