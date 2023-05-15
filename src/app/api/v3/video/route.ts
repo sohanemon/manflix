b23f907e4e16e1cb6a9df6869711178ee7f7802c
sohanemon <m.sohanemon@gmail.com>
2023-05-15 09:29:27 +0000

    May 15, 2023, 3:29 PM

--- src/app/api/v3/video/route.ts
+++ src/app/api/v3/video/route.ts
@@ -3,6 +3,9 @@ import { NextResponse } from 'next/server';
 import { JSDOM } from 'jsdom';
 import thumbnailToId from '@/util/thumbnail-to-id';
 
+export async function GET(req: Request) {
+  return NextResponse.json("Don't touch videos API");
+}
 export async function POST(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
