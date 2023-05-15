03b398d4819de86d623b4341d36ea8bb1b0ec865
sohanemon <m.sohanemon@gmail.com>
2023-05-15 09:16:50 +0000

    May 15, 2023, 3:16 PM

--- src/app/api/v2/carousel/route.ts
+++ src/app/api/v2/carousel/route.ts
@@ -3,3 +3,6 @@ import { NextResponse } from 'next/server';
 export async function GET(req: Request) {
   return NextResponse.json('data');
 }
+export async function POST(req: Request) {
+  return NextResponse.json('hey I am here');
+}
