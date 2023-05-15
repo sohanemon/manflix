0fca6e73e296e7dcd8ee3cf0291a1625c62bab7c
sohanemon <m.sohanemon@gmail.com>
2023-05-15 10:53:06 +0000

    May 15, 2023, 4:53 PM

--- src/app/api/v3/video/route.ts
+++ src/app/api/v3/video/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 
 import { JSDOM } from 'jsdom';
 import thumbnailToId from '@/util/thumbnail-to-id';
+import puppeteer from 'puppeteer';
 
 export async function GET(req: Request) {
   return NextResponse.json("Don't touch videos API");
@@ -11,10 +12,30 @@ export async function POST(req: Request) {
     const { searchParams } = new URL(req.url);
     const query = searchParams.get('query');
 
-    const response = await fetch(
-      process.env.SEARCH_URL + query?.replaceAll(' ', '+')!
-    );
-    const html = await response.text();
+    // puppeteer section
+    (async () => {
+      // Launch the browser
+      const browser = await puppeteer.launch({ headless: true });
+
+      // Create a page
+      const page = await browser.newPage();
+
+      // Go to your site
+      await page.goto(process.env.SEARCH_URL + query?.replaceAll(' ', '+')!);
+
+      // Query for an element handle.
+      const element = await page.waitForSelector('#primary > div.row.search');
+      console.log('🛑 ~ element:', element);
+
+      // Do something with element...
+      // await element?.click(); // Just an example.
+
+      // // Dispose of handle
+      // await element?.dispose();
+
+      // Close browser.
+      await browser.close();
+    })();
   } catch (error) {
     return NextResponse.json(error);
   }
